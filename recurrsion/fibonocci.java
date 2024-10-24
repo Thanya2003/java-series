cb92ab2295dd58a6aceb32670d77024320cda5fa
thanya2003 <thanyashettigar@gmail.com>
2024-10-24 12:16:18 +0530

    Recurrsion

---
+++ recurrsion/fibonocci.java
@@ -0,0 +1,20 @@
+package recurrsion;
+
+public class fibonocci {
+    public static void printfibo(int a, int b, int n){
+        if(n==0){
+            return;
+        }
+       int c=a+b;
+        System.out.print(c +" ");
+        printfibo(b, c, n-1);
+    }
+    public static void main(String[] args) {
+        int a=0;
+        int b=1;
+        int n=7;
+        System.out.print(a+" ");
+        System.out.print(b+" ");
+        printfibo(a, b, n-2);
+    }
+}
