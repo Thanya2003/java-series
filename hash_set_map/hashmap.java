6f6d4f986a122de97676a915942952d6d2f39a3a
thanya2003 <thanyashettigar@gmail.com>
2024-11-04 17:43:51 +0530

    hash-Map

---
+++ hash_set_map/hashmap.java
@@ -0,0 +1,26 @@
+package hash_set_map;
+
+import java.util.HashMap;
+
+public class hashmap {
+    public static void main(String[] args) {
+        HashMap <String, Integer> map=new HashMap<>();
+
+        map.put("India", 120);
+        map.put("China", 150);
+        map.put("Italy", 10);
+        map.put("US", 80);
+        System.out.println(map);
+
+        map.put("India", 160);
+        System.out.println(map);
+
+        if(map.containsKey("India")){
+            System.out.println("found");
+        }else{
+            System.out.println("Not found");
+        }
+        System.out.println(map.get("India"));
+        System.out.println(map.get("Indoneasia"));
+    }
+}
